d5ce24a1c516f4fe7cd00e0582234d9f4c804188
ziidat <achmadfauzi987@gmail.com>
2022-06-30 23:13:57 +0700

    tambah favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <title>Document</title>
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+    <link rel="icon" type="image/png" href="{{ asset('images/logo/peruri.svg') }}">
+    <title>Peruri E-Commerce</title>
 </head>
 <body>
     @include('layouts.navbar')
