20a781e9d7d8c2e182a5b9183f4dbc2688681756
vixia05 <vikiryan0@gmail.com>
2022-07-01 14:31:49 +0700

    Revisi Detail Product

--- resources/views/dummy/dummy-kategori.blade.php
+++ resources/views/dummy/dummy-kategori.blade.php
@@ -1,60 +1,56 @@
 <div class="inline-block px-3">
     <div class="min-w-fit">
-        <a href="" class="w-full block bg-white shadow-md hover:shadow-xl rounded-lg overflow-hidden">
-            <div class="relative pb-10 overflow-hidden">
-                <img class="object-none object-center bg-yellow-300 w-24 h-24"
-                    src="{{ asset('images/logo/makanan.svg') }}"
-                    alt="">
+        <a href="" class="w-full border block bg-white shadow-md hover:shadow-xl rounded-lg overflow-hidden">
+            <div class="relative overflow-hidden">
+                <img class="object-fit object-center bg-slate-300 w-24 h-24"
+                    src="{{ asset('images/logo/makanan.svg') }}" alt="">
             </div>
-            <div class="p-4 ">
-                <h4 class="text-center text-sm font-medium">Makanan</h4>
+            <div class="p-3">
+                <h4 class="text-center text-sm font-semibold">Makanan</h4>
             </div>
-            
+
         </a>
     </div>
 </div>
 <div class="inline-block px-3">
     <div class="min-w-fit">
         <a href="" class="w-full block bg-white shadow-md hover:shadow-xl rounded-lg overflow-hidden">
-            <div class="relative pb-10 overflow-hidden">
-                <img class="object-none object-center bg-yellow-300 w-24 h-24"
-                    src="{{ asset('images/logo/elektronik.svg') }}"
-                    alt="">
+            <div class="relative  overflow-hidden">
+                <img class="object-fit object-center bg-slate-300 w-24 h-24"
+                    src="{{ asset('images/logo/elektronik.svg') }}" alt="">
             </div>
-            <div class="p-4 ">
-                <h4 class="text-center text-sm font-medium">Elektronik</h4>
+            <div class="p-3">
+                <h4 class="text-center text-sm font-semibold">Elektronik</h4>
             </div>
-            
+
         </a>
     </div>
 </div>
 <div class="inline-block px-3">
     <div class="min-w-fit">
         <a href="" class="w-full block bg-white shadow-md hover:shadow-xl rounded-lg overflow-hidden">
-            <div class="relative pb-10 overflow-hidden">
-                <img class="object-none object-center bg-yellow-300 w-24 h-24"
-                    src="{{ asset('images/logo/jasa.svg') }}"
+            <div class="relative  overflow-hidden">
+                <img class="object-fit object-center bg-slate-300 w-24 h-24" src="{{ asset('images/logo/jasa.svg') }}"
                     alt="">
             </div>
-            <div class="p-4 ">
-                <h4 class="text-center text-sm font-medium">Jasa</h4>
+            <div class="p-3">
+                <h4 class="text-center text-sm font-semibold">Jasa</h4>
             </div>
-            
+
         </a>
     </div>
 </div>
 <div class="inline-block px-3">
     <div class="min-w-fit">
         <a href="" class="w-full block bg-white shadow-md hover:shadow-xl rounded-lg overflow-hidden">
-            <div class="relative pb-10 overflow-hidden">
-                <img class="object-none object-center bg-yellow-300 w-24 h-24"
-                    src="{{ asset('images/logo/pakaian.svg') }}"
-                    alt="">
+            <div class="relative  overflow-hidden">
+                <img class="object-fit object-center bg-slate-300 w-24 h-24"
+                    src="{{ asset('images/logo/pakaian.svg') }}" alt="">
             </div>
-            <div class="p-4 ">
-                <h4 class="text-center text-sm font-medium">Pakaian</h4>
+            <div class="p-3">
+                <h4 class="text-center text-sm font-semibold">Pakaian</h4>
             </div>
-            
+
         </a>
     </div>
 </div>
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,70 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
-    {{-- Header --}}
-    <div class="flex justify-center mx-auto border-b">
-        <div class="block max-w-7xl text-center p-8 my-10">
-            <h3 class="font-semibold leading-tight text-3xl my-auto mb-3">Produk Original</h3>
-            <p class="font-normal leading-tight text-base text-gray-600 opacity-80 my-auto">Lorem ipsum dolor sit amet
-                consectetur, adipisicing elit. Laborum, accusamus, vel quas earum quo facere hic sunt adipisci quisquam
-                excepturi minus quasi inventore sint ipsum harum delectus rerum eos repudiandae!</p>
+    <div class="flex justify-center mx-auto mt-10 shadow-lg rounded-xl w-fit pb-8">
+        <div class="max-w-lg p-4">
+            <img src="{{ asset('images/motor.webp') }}" class="rounded-md shadow-md object-cover">
         </div>
-    </div>
-    {{-- End Header --}}
-    {{-- Sidebar Category --}}
-    <div class="flex justify-center mx-auto px-10">
-        <div class="hidden md:block max-w-md min-w-fit mt-10 shadow-lg border p-5 rounded-md">
-            <h5 class="font-bold leading-tight text-base my-auto p-3">Categories</h5>
-            <div class="px-4 mt-2 space-y-2">
-                <div class="form-check">
-                    <input
-                        class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                        type="checkbox" value="" id="categories1" checked>
-                    <label class="form-check-label text-sm inline-block text-gray-7 00" for="categories1">
-                        Produk Original
-                    </label>
-                </div>
-                <div class="form-check">
-                    <input
-                        class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                        type="checkbox" value="" id="categories2">
-                    <label class="form-check-label text-sm inline-block text-gray-700" for="categories2">
-                        Makanan
-                    </label>
-                </div>
-                <div class="form-check">
-                    <input
-                        class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                        type="checkbox" value="" id="categories3">
-                    <label class="form-check-label text-sm inline-block text-gray-700" for="categories3">
-                        Otomotif
-                    </label>
-                </div>
-                <div class="form-check">
-                    <input
-                        class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                        type="checkbox" value="" id="categories4">
-                    <label class="form-check-label text-sm inline-block text-gray-700" for="categories4">
-                        Jasa
-                    </label>
-                </div>
-                <div class="form-check">
-                    <input
-                        class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                        type="checkbox" value="" id="categories5">
-                    <label class="form-check-label text-sm inline-block text-gray-700" for="categories5">
-                        Pakaian
-                    </label>
+        <div class="max-w-full">
+            <div class="flex justify-center">
+                <div class="block p-4 bg-white max-w-2xl min-w-fit">
+                    <h5 class="text-gray-900 text-2xl leading-tight font-extrabold mb-3">Motor Racing</h5>
+                    <p class="text-gray-900 text-2xl leading-snug font-medium mb-3">Rp.125.000.000</p>
+                    <p class="text-gray-700 text-base mb-10 leading-normal">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo libero perferendis est error? Omnis
+                        odit neque commodi laborum culpa alias, veniam similique molestias, excepturi sequi reiciendis
+                        doloremque ipsa labore eum amet mollitia! Porro quas optio dolore. Facilis sed minus, ducimus
+                        similique fugiat, dicta esse nihil, illo iusto suscipit voluptatem error.
+                    </p>
+                    <p class="text-gray-700 text-sm leading-tight font-medium">
+                        Color
+                    </p>
+                    <div class="form-check space-x-3 mb-6">
+                        <input
+                            class="form-check-input rounded-full appearance-none h-8 w-8 border hover:black border-gray-300 bg-white checked:bg-black checked:border-black focus:outline-none transition duration-200 my-1 align-top bg-no-repeat bg-center bg-contain float-left cursor-pointer"
+                            type="checkbox" value="" id="flexCheckDefault3" checked>
+                        <input
+                            class="form-check-input appearance-none h-8 w-8 border border-gray-300 rounded-full bg-white active::bg-black checked:bg-yellow-600 checked:border-yellow-600 focus:outline-none transition duration-200 my-1 align-top bg-no-repeat bg-center bg-contain float-left cursor-pointer"
+                            type="checkbox" value="" id="flexCheckChecked3">
+                    </div>
+                    <div class="block pt-14">
+                        <button type="button" class="mb-2 w-1/2 inline-block px-6 py-2.5 bg-green-500 rounded shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-700 active:shadow-lg transition duration-150 ease-in-out">
+                        <img src="{{asset('images/logo/whatsapp.svg')}}" class="w-8 h-8 inline-block mr-2 text-white">
+                        <span class="text-lg font-medium leading-normal text-white">Hubungi Penjual</span></button>
+                    </div>
                 </div>
             </div>
         </div>
-        {{-- End Sidebar Category --}}
-        {{-- Item Content --}}
-        <div class="block max-w-fit mt-2.5 shadow-lg p-8">
-            <div class="grid grid-flow-row xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-6">
-                @include('dummy.dummy-category')
-            </div>
-        </div>
-        {{-- End Item Content --}}
     </div>
 @endsection
